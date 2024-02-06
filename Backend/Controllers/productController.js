65e03fb082e79273d1bea70b1fe910075fde1cde
Shamanth K Murthy <shamanthk2404@gmail.com>
2024-02-06 09:36:29 +0530

    category

---
+++ Backend/Controllers/productController.js
@@ -0,0 +1,40 @@
+import {db} from '../index.js'
+
+export const productInput = (req, res) => {
+  const { name, description, category, price, stock_quantity, weight } =
+    req.body;
+
+  db.query(
+    "insert into products (name, description, category, price, stock_quantity, weight) values(?,?,?,?,?,?)",
+    [name, description, category, price, stock_quantity, weight],
+    (err, data) => {
+      if (err) return res.json(err);
+      console.log("Successful");
+      return res.json(data);
+    }
+  );
+};
+
+export const productDisplay = (req,res) =>{  
+  db.query('select * from products where category = ?',[req.query.category], (err, result) => {
+    if (err) {
+      console.error("MySQL query error:", err);
+      res.status(500).send("Error fetching data from database");
+    } else {
+      console.log(result);
+      res.json(result);
+    }
+  });
+}
+
+export const productDetailsDisplay = (req,res) =>{  
+  db.query('select * from products where name = ?',[req.query.product], (err, result) => {
+    if (err) {
+      console.error("MySQL query error:", err);
+      res.status(500).send("Error fetching data from database");
+    } else {
+      console.log(result);
+      res.json(result);
+    }
+  });
+}
--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mysql from "mysql2";
 import cors from "cors";
 import authRouter from './Routes/auth.js'
+import productRouter from './Routes/product.js';
+import accountRouter from './Routes/account.js';
 
 const app = express();
 
@@ -15,6 +17,8 @@ export const db = mysql.createConnection({
 app.use(express.json());
 app.use(cors());
 app.use(authRouter);
+app.use(productRouter);
+app.use(accountRouter);
 
 app.listen(8800, () => {
     console.log("Connected backend");
