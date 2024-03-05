62f41700a1fa09507b8b9c1031f159f2a0df2856
Shamanth K Murthy <shamanthk2404@gmail.com>
2024-03-05 17:45:16 +0530

    Cart Items Increent & Decrement

--- Backend/Controllers/cartController.js
+++ Backend/Controllers/cartController.js
@@ -39,7 +39,7 @@ export const cartItemsDisplay = (req, res) => {
 };
 
 export const placeOrder = (req, res) => {
-  const { email, items, totalPrice } = req.body;
+  const { email,totalPrice } = req.body;
   db.query(
     "insert into orders(user_email,total_amount) values(?,?)",
     [email, totalPrice],
@@ -48,50 +48,107 @@ export const placeOrder = (req, res) => {
         console.error("MySQL query error:", err);
         res.status(500).send("Error fetching data from database");
       } else {
-        console.log(result);
-
-        db.query(
-          "select order_id from orders where user_email = ?",
-          [email],
-          (err, result1) => {
-            if (err) {
-              console.error("MySQL query error:", err);
-              res.status(500).send("Error fetching data from database1");
-            } else {
-              console.log(result1);
-
-              items.forEach((item) => {
-                db.query(
-                  "insert into order_items(order_id,product_name,quantity,price,totalPrice) values(?,?,?,?,?)",
-                  [result1[0].order_id,item.productName,item.quantity,item.price,totalPrice],
-                  (err, result2) => {
-                    if (err) {
-                      console.error("MySQL query error:", err);
-                      res.status(500).send("Error fetching data from database2");
-                    } else {
-                      console.log(result2);
-
-                      db.query(
-                        "delete from carts where email = ?",
-                        [email],
-                        (err, result3) => {
-                          if (err) {
-                            console.error("MySQL query error:", err);
-                            res.status(500).send("Error fetching data from database3");
-                          } else {
-                            console.log(result3);
-                            res.json(result3);
-                          }
-                        }
-                      );
-                    }
-                  }
-                );
-              });
-            }
-          }
-        );        
+        console.log(result);      
+        res.json(result);          
       }
     }
   );
 };
+
+export const orderItems = (req,res) => {
+  const { email,items ,totalPrice } = req.body;
+  db.query(
+    "select order_id from orders where user_email = ?",
+    [email],
+    (err, result1) => {
+      if (err) {
+        console.error("MySQL query error:", err);
+        res.status(500).send("Error fetching data from database1");
+      } else {        
+        items.forEach((item) => {
+          db.query(
+            "insert into order_items(order_id,product_name,quantity,price,totalPrice) values(?,?,?,?,?)",
+            [result1[0].order_id,item.productName,item.quantity,item.price,totalPrice],
+            (err, result2) => {
+              if (err) {
+                console.error("MySQL query error:", err);
+                res.status(500).send("Error fetching data from database2");
+              } else {
+                console.log(result2);                             
+              }
+            }
+          );
+        });
+        res.json(result1);
+      }
+    }
+  );
+}
+
+export const deleteCart = (req,res) =>{
+  const { email } = req.body;
+  db.query(
+    "delete from carts where email = ?",
+    [email],
+    (err, result) => {
+      if (err) {
+        console.error("MySQL query error:", err);
+        res.status(500).send("Error fetching data from database3");
+      } else {
+        console.log(result);
+        res.json(result);
+      }
+    }
+  );
+}
+
+export const incrementQuantity = (req,res) => {
+  const {email,productName} = req.body;
+  db.query(
+    "update carts set quantity = quantity + 1 where email = ? and productName = ?",
+    [email,productName],
+    (err, result) => {
+      if (err) {
+        console.error("MySQL query error:", err);
+        res.status(500).send("Error fetching data from database3");
+      } else {
+        console.log(result);
+        res.json(result);
+      }
+    }
+  );
+}
+
+export const decrementQuantity = (req,res) => {
+  const {email,productName} = req.body;
+  db.query(
+    "update carts set quantity = quantity - 1 where email = ? and productName = ?",
+    [email,productName],
+    (err, result) => {
+      if (err) {
+        console.error("MySQL query error:", err);
+        res.status(500).send("Error fetching data from database3");
+      } else {
+        console.log(result);
+        res.json(result);
+      }
+    }
+  );
+}
+
+export const deleteItem = (req,res) =>{
+  const { email , productName} = req.body;
+  db.query(
+    "delete from carts where email = ? and productName = ?",
+    [email,productName],
+    (err, result) => {
+      if (err) {
+        console.error("MySQL query error:", err);
+        res.status(500).send("Error fetching data from database3");
+      } else {
+        console.log(result);
+        res.json(result);
+      }
+    }
+  );
+}
